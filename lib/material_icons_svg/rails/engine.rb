00c7a5f01675a7fab2778c5261fed2eb30af5424
Ian Li <ianarli@gmail.com>
2022-09-25 23:36:32 -0400

    Add *.svg in assets.precompile

--- lib/material_icons_svg/rails/engine.rb
+++ lib/material_icons_svg/rails/engine.rb
@@ -8,6 +8,13 @@ module MaterialIconsSvg
         # Copy the whole repository, so that we have these information:
         # license, version, README.
         app.config.assets.paths << root.join('assets', 'material-icons', 'svg').to_s
+
+        # Fix problem with "SVG not found" in production
+        # @see https://github.com/barrymieny/material_design_icons/issues/6#issuecomment-503060905
+        #
+        # Do you need to set a specific path? NO.
+        # @see https://github.com/rails/sprockets/issues/542#issue-288657474
+        app.config.assets.precompile += ['*.svg']
       end
     end
   end
